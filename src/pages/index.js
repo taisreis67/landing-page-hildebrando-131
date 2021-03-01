a5140779a9df5cd4e1af5793e7329a27f3eb1da2
Tais Duarte <tais.duarte@creditas.com.br>
2021-03-01 20:23:01 -0300

    [ meta ] add styled componet in project.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,11 +7,16 @@ import Hero from "../components/hero"
 import Content from "../components/content"
 import CallToAction from "../components/cta"
 
+const Excerpt = styled.p`
+  background-color: red;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO />
     <Hero />
     <Content />
+    <Excerpt>Chuchu</Excerpt>
     <CallToAction />
   </Layout>
 )
