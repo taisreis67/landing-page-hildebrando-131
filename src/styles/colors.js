ba47a095da37670e634c8ea07a4669b970ce5e93
Tais Duarte <tais.duarte@creditas.com.br>
2021-03-12 00:23:11 -0300

    [ refac ] change the default colors.

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,14 +1,6 @@
 export default {
 	white: '#ffffff',
-	yellow: '#FFCF0D',
-	orange: '#FF7E2E',
-	green: '#57CB25',
-	balckGreen: '#14BD85',
-	blue: '#4D72EF',
-	cleanBlue: '#38B6FF',
-	blackBlue: '#1D11B2',
-	rose: '#FF66C4',
-	gray: '#6c6c6c',
 	browm: '#4c3426',
 	gold: '#ffc695',
+	darkBrowm: '#ffc695',
 };
