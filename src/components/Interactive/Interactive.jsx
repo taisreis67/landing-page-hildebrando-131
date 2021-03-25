8bbb1cea7f6c2b81089de7a17c7683a9fb0d98a1
Tais Duarte <tais.duarte@creditas.com.br>
2021-03-25 00:31:33 -0300

    [ refac ] remove the title in Interactive Component.

--- src/components/Interactive/Interactive.jsx
+++ src/components/Interactive/Interactive.jsx
@@ -3,21 +3,12 @@ import { Box } from "grommet"
 
 import { StyledInteractive } from './Interactive.style'
 
-const Banner = ({ background, children }) => {
-  const title = children[0]
-  const interactiveElement = children[1]
+const Interactive = ({ background, children }) => (
+  <StyledInteractive pad={{ vertical: "large"}} background={background}>
+    <Box width="xlarge" margin="auto" pad="medium">
+      {children}
+    </Box>
+  </StyledInteractive>
+)
 
-  return (
-    <>
-      {title}
-
-      <StyledInteractive pad={{ vertical: "large"}} background={background}>
-        <Box width="xlarge" margin="auto" pad="medium">
-          {interactiveElement}
-        </Box>
-      </StyledInteractive>
-    </>
-  )
-}
-
-export default Banner
+export default Interactive
--- src/components/Interactive/Interactive.test.jsx
+++ src/components/Interactive/Interactive.test.jsx
@@ -3,23 +3,21 @@ import { render, screen } from "@testing-library/react"
 
 import Interactive from "."
 
-describe("Banner", () => {
+describe("Interactive", () => {
   it("renders correctly", async () => {
     render(
       <Interactive background="#fff">
-        <div>Tour Virtual</div>
-
         <iframe
           src="https://www.3dexplora.com.br/seutour.aspx?codigo=q5kYoNLfgYv&amp;play=1&amp;hl=0&amp;qs=1&amp;wh=1&amp;lp=0&amp;ts=1"
           allowFullScreen=""
           width="100%"
           height="500px"
           frameBorder="0"
-          title="hildebrando 131 tour virtual"
+          title="Hildebrando 131 tour virtual"
         />
       </Interactive>
     )
 
-    expect(screen.getByText(/Tour Virtual/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/Hildebrando 131 tour virtual/i)).toBeInTheDocument()
   })
 })
