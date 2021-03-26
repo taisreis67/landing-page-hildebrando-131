35720b3511bc060269bf960ba9609f713b5402f9
Tais Duarte <tais.duarte@creditas.com.br>
2021-03-26 19:13:10 -0300

    [ refac ] change font size to be responsive at menu.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,38 @@
-import React from "react"
-import { Box, Text } from "grommet";
+import React, { useContext } from "react"
+import { Box, Text, ResponsiveContext } from "grommet";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 import { StyledFooter, StyledAnchor } from './Footer.style'
 
-const Footer = ({ background, color }) => (
-  <StyledFooter background={background}>
-    <Box width="medium" margin="auto">
-      <StyledAnchor
-        href="https://api.whatsapp.com/send?phone=5511967147840"
-        target="blank"
-        color={color}
-      >
-        <FontAwesomeIcon
-          icon={faWhatsapp}
-          size="2x"
-        />
+const Footer = ({ background, color }) => {
+  const size = useContext(ResponsiveContext);
 
-        <Text textAlign="left" alignSelf="center" margin={{ left: 'small'}} color="#fff">Entre em contato para saber mais</Text>
-      </StyledAnchor>
-    </Box>
-  </StyledFooter>
-);
+  return (
+    <StyledFooter background={background}>
+      <Box width="medium" margin="auto">
+        <StyledAnchor
+          href="https://api.whatsapp.com/send?phone=5511967147840"
+          target="blank"
+          color={color}
+        >
+          <FontAwesomeIcon
+            icon={faWhatsapp}
+            size="2x"
+          />
+
+          <Text
+            textAlign="left"
+            alignSelf="center"
+            margin={{ left: 'small'}}
+            color="#fff"
+            size={size !== 'small' ? 'medium' : 'small'}>
+              Entre em contato para saber mais
+            </Text>
+        </StyledAnchor>
+      </Box>
+    </StyledFooter>
+  )
+}
 
 export default Footer;
