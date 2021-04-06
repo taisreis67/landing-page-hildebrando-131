b1ee5428f934e303f6125831093f9a02e0b4c14a
Tais Duarte <tais.duarte@creditas.com.br>
2021-04-05 22:56:12 -0300

    [ feature ] change sitemap config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Empreendimneto Hildebrando 131, apartamentos para compra na avenida Hildebrando de Lima, Osasco - SP`,
     keywords: `hildebrando de lima, apartamento, osasco, são paulo, imóveis, compra, investimeto em imóveis, minha casa minha vida, casa verde amarela, apartementos em osasco, direções imóveis`,
     author: `@taisreis67`,
-    siteUrl: `https://www.hildebrando131.com/`,
+    siteUrl: `https://hildebrando131.com`,
     googleSiteVerification: `lPPRsKAJ8o2Qe7dQq5XmgM5RlBOqQvMDIt5jk5eZX_k`
   },
   plugins: [
